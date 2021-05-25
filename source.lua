aa4ffc6c6026cd23d152eafb058af84c6a6b2591
Alex Liu <30390986+applebee1558@users.noreply.github.com>
2021-05-24 22:28:56 -0700

    remove dogix from copyright as he doesn't actually own bark now

--- source.lua
+++ source.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright 2020 - 2021, applebee, dogix and the Bark contributors
+Copyright 2020 - 2021, applebee and the current Bark developers
 This source is controlled by the AGPLv3 License. 
 Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. 
 Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.
